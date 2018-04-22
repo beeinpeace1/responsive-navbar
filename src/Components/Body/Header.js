c6f095ee13a27ee93c7655ae608a9a4597d9adf0
Maya <beeinpeace1@gmail.com>
2018-04-22 13:09:54 +0530

    added fade in to header text

--- src/Components/Body/Header.js
+++ src/Components/Body/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
   render() {
     return (
       <div className="Header">
-        <div className="text-container">
+        <div className="text-container animated fadeIn">
             <h3>Le Todo</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, laborum!</p>
         </div>
