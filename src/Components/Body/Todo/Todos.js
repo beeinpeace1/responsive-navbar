f7ace559bf9a5ac1a8c4bc8e2a8c01588f54bc08
Maya <beeinpeace1@gmail.com>
2018-04-22 18:48:27 +0530

    using jsonplaceholder for todo lists

--- src/Components/Body/Todo/Todos.js
+++ src/Components/Body/Todo/Todos.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './Todos.css';
 import Todo from './Todo';
 import AddTodo from './AddTodo'
+var loremIpsum = require('lorem-ipsum')
 
 class Todos extends Component {
   constructor(props) {
@@ -10,34 +11,7 @@ class Todos extends Component {
   }
 
   state = {
-    todos: [{
-      title: "Todo Item 1",
-      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam",
-      completed: false
-    }, {
-      title: "Todo Item 2",
-      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam",
-      completed: true
-    }, {
-      title: "Todo Item 3",
-      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam",
-      completed: true
-    },
-    {
-      title: "Todo Item 4",
-      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam",
-      completed: true
-    },
-    {
-      title: "Todo Item 5",
-      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam",
-      completed: true
-    },
-    {
-      title: "Todo Item 6",
-      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam",
-      completed: true
-    }],
+    todos: [],
   };
 
   updateTodo(e, i, t) {
@@ -57,27 +31,40 @@ class Todos extends Component {
         })
         break;
       case 'addtodo':
-      const oldTodo = this.state.todos;
-      oldTodo.push(i);
-      this.setState({
-        todos: oldTodo
-      })
-      break;
+        const oldTodo = this.state.todos;
+        oldTodo.push(i);
+        this.setState({
+          todos: oldTodo
+        })
+        break;
       default:
         console.log("Default");
     }
   }
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(e => e.json())
+    .then(res => {
+      this.setState({
+        todos: res
+      })
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
   render() {
     return (
       <div>
-        <AddTodo updateTodo={this.updateTodo}/>
+        <AddTodo updateTodo={this.updateTodo} />
         <div className="Todos-Container">
           <h3 className="list-of-todos">
             <span>List of Todos</span>
-            <i className="octicon octicon-plus" onClick={(e)=>{
-                  document.querySelector('.AddTodo').classList.remove('AddTodoShow');
-              }}></i>
+            <i className="octicon octicon-plus" onClick={(e) => {
+              document.querySelector('.AddTodo').classList.remove('AddTodoShow');
+            }}></i>
           </h3>
           <div className="todos-row">
             {this.state.todos.map((object, index) => {
@@ -85,7 +72,7 @@ class Todos extends Component {
                 id={index}
                 key={object.title + index}
                 title={object.title}
-                body={object.body}
+                body={object.body || loremIpsum()}
                 updateTodo={this.updateTodo}
                 completed={object.completed}
               />
