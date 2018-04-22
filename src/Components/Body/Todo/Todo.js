5049a7a94b2d108171118ad339d9e9937359fcbc
Maya <beeinpeace1@gmail.com>
2018-04-22 15:25:51 +0530

    removed class toggling, rending completed todos conditionally

--- src/Components/Body/Todo/Todo.js
+++ src/Components/Body/Todo/Todo.js
@@ -4,15 +4,14 @@ import './Todo.css';
 class Todo extends Component {
     render() {
         return (
-            <div className="Todo-Container">
+            <div className={"Todo-Container " + (this.props.completed ? "completed" : "") }>
                 <div className="Todo-Container-Top">
-                    <h3>Todo Item 1</h3>
-                    <p>Lorem ipsum dolor sit amet.</p>
+                    <h3>{this.props.title}</h3>
+                    <p>{this.props.body}</p>
                 </div>
                 <div className="Todo-Container-Bottom">
                     <i title="completed" className="octicon octicon-check" onClick={(e)=>{
-                        e.preventDefault();
-                        e.target.parentNode.parentNode.classList.toggle('completed');
+                        this.props.updateTodo(e, this.props.id, 'completed');
                     }}></i>
                     <i title="delete" className="octicon octicon-x"></i>
                 </div>
--- src/Components/Body/Todo/Todos.js
+++ src/Components/Body/Todo/Todos.js
@@ -3,16 +3,53 @@ import './Todos.css';
 import Todo from './Todo';
 
 class Todos extends Component {
+  constructor(props) {
+    super(props);
+    this.updateTodo = this.updateTodo.bind(this);
+  }
+
+  state = {
+    todos: [{
+      title: "Todo Item 1",
+      body: "loem ipsum data bodu adu idu",
+      completed: false
+    },{
+      title: "Todo Item 2",
+      body: "loem ipsum data bodu adu idu",
+      completed: true
+    }],
+  };
+
+  updateTodo(e, i, t) {
+    e.preventDefault();
+    switch(t){
+      case 'completed':
+        const todos = this.state.todos;
+        todos[i].completed = !todos[i].completed
+        this.setState({
+          todos: todos
+        })
+      break;
+      default:
+        console.log("sds");
+    }
+  }
+
   render() {
+    console.log(this.state);
     return (
       <div className="Todos-Container">
         <div className="todos-row">
-          <Todo/>
-          <Todo/>
-          <Todo/>
-          <Todo/>
-          <Todo/>
-          <Todo/>
+          {this.state.todos.map((object, index)=>{
+            return <Todo 
+            id={index} 
+            key={object.title + index} 
+            title={object.title} 
+            body={object.body} 
+            updateTodo={this.updateTodo}
+            completed={object.completed}
+            />
+          })}
         </div>
       </div>
     );
