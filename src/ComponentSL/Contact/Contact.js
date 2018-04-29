62e383c6093f4b5c3889ca304230dbaa65423613
maya <beeinpeace1@gmail.com>
2018-04-29 11:10:32 +0530

    removed about and add contact

--- src/ComponentSL/Contact/Contact.js
+++ src/ComponentSL/Contact/Contact.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
 import './Contact.css';
 
+class ScrollToTopOnMount extends Component {
+  componentDidMount() {
+    window.scrollTo(0, 0)
+  }
+
+  render() {
+    return null
+  }
+}
+
 const Contact = () => {
   return (
+
     <div className="Contact">
-      <div className="animated fadeIn">
-        <h3>Contact</h3>
-      </div>
+      <ScrollToTopOnMount/>  
+      <div className="details-container">
+        <div className="image-holder">
+          <img src="bee.jpg" alt="" width={250} height={250} />
+        </div>  
+        <div className="details">
+          <h3>Mala S</h3>
+          <span className="octicon octicon-mail">&nbsp;
+            <a href="mailto:beeinpeace1@gmail.com">beeinpeace1@gmail.com</a>
+          </span>
+          <span className="octicon octicon-device-mobile">&nbsp;
+            <a href="tel:+918861568805">+918861568805</a>
+          </span>
+          <span className="octicon octicon-mark-github">&nbsp;
+            <a href="https://github.com/beeinpeace1" target="__blank">https://github.com/beeinpeace1</a>
+          </span>
+          <span className="octicon octicon-file-pdf">&nbsp;
+            <a href="resume_mala.pdf" target="__blank">Resume</a>
+          </span>
+        </div>
+      </div>  
     </div>
   );
 }
--- src/ComponentSL/Navigation/Navigation.js
+++ src/ComponentSL/Navigation/Navigation.js
@@ -21,7 +21,6 @@ const Navigation = () => {
           <ul>
               <li><Link to="/">Home</Link></li>
               <li><Link to="/contact">Contact</Link></li>
-              <li><Link to="/about">About</Link></li>
           </ul>
         </div>
       </div>
@@ -29,7 +28,6 @@ const Navigation = () => {
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/contact">Contact</Link></li>
-            <li><Link to="/about">About</Link></li>
           </ul>
         </div>
     </nav>
